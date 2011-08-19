acaf82cc03b9d70343f7034b9938e5581026637b
Ben Alman <cowboy@rj3.net>
2011-08-19 10:47:40 -0400

    Slight test reorganization.

--- spec/gaucho/pageset_spec.rb
+++ spec/gaucho/pageset_spec.rb
@@ -8,46 +8,55 @@ module Gaucho
         @pageset = Pageset.new 'spec/fixtures/basic-tests'
       end
 
-      it "#repo: should be a Grit::Repo" do
-        # passing in a path
-        @pageset.repo.should.instance_of? Grit::Repo
-        # passing in a Grit::Repo instance
-        repo = Grit::Repo.new 'spec/fixtures/basic-tests'
-        pageset = Pageset.new repo
-        pageset.repo.should.instance_of? Grit::Repo
+      describe "#repo" do
+        it "should be a Grit::Repo (passing in a path)" do
+          @pageset.repo.instance_of?(Grit::Repo).should eq true
+        end
+        it "should be a Grit::Repo (passing in a repo)" do
+          repo = Grit::Repo.new 'spec/fixtures/basic-tests'
+          pageset = Pageset.new repo
+          pageset.repo.instance_of?(Grit::Repo).should eq true
+        end
       end
 
-      it "#page_commits: should parse the commit log properly" do
-        # note: this is overly implementation-specific
-        @pageset.page_commits.all? do |id, commits|
-          id =~ /^page-\d+$/ &&
-            commits.count == 2 &&
-            commits.first[:files].count == 1 &&
-            commits.last[:files].count == 2
-        end.should == true
+      describe "#page_commits" do
+        it "should parse the commit log properly" do
+          # note: this is overly implementation-specific
+          @pageset.page_commits.all? do |id, commits|
+            id =~ /^page-\d+$/ &&
+              commits.count == 2 &&
+              commits.first[:files].count == 1 &&
+              commits.last[:files].count == 2
+          end.should eq true
+        end
       end
 
-      it "#[]: should allow access to pages by their id" do
-        @pageset['page-1'].instance_of? Gaucho::Page
-        @pageset['page-99'].instance_of? Gaucho::Page
+      describe "#[]" do
+        it "should allow access to pages by their id" do
+          @pageset['page-1'].instance_of?(Gaucho::Page).should eq true
+          @pageset['nonexistent'].should eq nil
+        end
       end
 
-      it "#pages: should allow access to all pages" do
-        @pageset.pages.count.should == 10
-        @pageset.pages.all? {|page| page.instance_of? Gaucho::Page}.should == true
+      describe "#pages" do
+        it "should allow access to all pages" do
+          @pageset.pages.count.should eq 10
+          @pageset.pages.all? {|page| page.instance_of? Gaucho::Page}.should eq true
+        end
       end
 
-      it "#each (etc): should behave like an Enumerable" do
+      it "should behave like an Enumerable" do
         # Enumeration w/ block
-        @pageset.inject(0) {|sum, page| sum += 1}.should == 10
+        @pageset.inject(0) {|sum, page| sum += 1}.should eq 10
         # Enumerator w/o block
-        @pageset.each.inject(0) {|sum, page| sum += 1}.should == 10
+        @pageset.each.inject(0) {|sum, page| sum += 1}.should eq 10
         # Etc
-        @pageset.count.should == 10
-        @pageset.first.should.instance_of? Gaucho::Page
-        @pageset.all? {|page| page.instance_of? Gaucho::Page}.should == true
+        @pageset.count.should eq 10
+        @pageset.first.instance_of?(Gaucho::Page).should eq true
+        @pageset.all? {|page| page.instance_of? Gaucho::Page}.should eq true
       end
 
+      it "should default to sorting by commit date"
 
     end
   end
